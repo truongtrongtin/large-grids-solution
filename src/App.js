7cb1c8ced35935b5061fe30dc014862d8c86ec61
truongtrongtin <truongtrongtin0305@gmail.com>
2020-03-19 20:45:53 +0700

    Solve problem using react-window

--- src/App.js
+++ src/App.js
@@ -1,26 +1,119 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useMemo, useEffect } from 'react';
 import './App.css';
+import AutoSizer from "react-virtualized-auto-sizer";
+import { FixedSizeGrid as Grid } from 'react-window';
+
+function Cell({ columnIndex, rowIndex, data, style }) {
+  const { selection, setSelection } = data;
+  const { start, end } = selection;
+
+  function className() {
+    // Check if cell is outside of selected area
+    if (columnIndex < Math.min(start.columnIndex, end.columnIndex)
+      || columnIndex > Math.max(start.columnIndex, end.columnIndex)
+      || rowIndex < Math.min(start.rowIndex, end.rowIndex)
+      || rowIndex > Math.max(start.rowIndex, end.rowIndex)) {
+      return '';
+    }
+    return 'selected';
+  }
+
+  function handleMouseDown({rowIndex, columnIndex}) {
+    setSelection(prevState => ({
+      ...selection,
+      isSelecting: true,
+      start: {
+        ...prevState.start,
+        rowIndex,
+        columnIndex
+      },
+      end: {
+        ...prevState.end,
+        rowIndex,
+        columnIndex
+      }
+    }));
+  }
+
+  function handleMouseOver({rowIndex, columnIndex}) {
+    if (selection.isSelecting) {
+      setSelection(prevState => ({
+        ...selection,
+        end: {
+          ...prevState.end,
+          rowIndex,
+          columnIndex
+        }
+      }));
+    }
+  }
+
+  return (
+    <div
+      className={className()}
+      onMouseDown={() => handleMouseDown({rowIndex, columnIndex})}
+      onMouseOver={() => handleMouseOver({rowIndex, columnIndex})}
+      style={{
+        ...style,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        border: '.5px solid black',
+        userSelect: 'none',
+      }}
+    >
+      {columnIndex}, {rowIndex}
+    </div>
+  )
+};
 
 function App() {
+  const [selection, setSelection] = useState({
+    isSelecting: false,
+    start: {
+      rowIndex: -1,
+      columnIndex: -1
+    },
+    end: {
+      rowIndex: -1,
+      columnIndex: -1
+    }
+  });
+
+  function handleMouseUp() {
+    setSelection(prevState => ({
+      ...prevState,
+      isSelecting: false,
+    }));
+  }
+
+  useEffect(() => {
+    document.addEventListener('mouseup', handleMouseUp);
+    return () => document.removeEventListener('mouseup', handleMouseUp);
+  })
+
+  const itemData = useMemo(() => ({
+    selection,
+    setSelection
+  }), [selection]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <AutoSizer>
+      {({ height, width }) => (
+        <Grid
+          columnCount={1000}
+          columnWidth={100}
+          height={height}
+          rowCount={1000000}
+          rowHeight={35}
+          width={width}
+          itemData={itemData}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          {Cell}
+        </Grid>
+      )}
+    </AutoSizer>
+  )
+};
 
 export default App;
